faff53fc46927fd06fc7e812c2e56499f7b95edb
Joao Henrique Machado Silva <joaoh82@gmail.com>
2016-11-15 23:30:27 -0200

    Use of defering with time package

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
+func main() {
+	if len(os.Args) > 1 {
+		defer fmt.Print(os.Args[1], ".\n")
+	}
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Print("Good morning, ")
+	case t.Hour() < 18:
+		fmt.Print("Good afternoon, ")
+	default:
+		fmt.Print("Good evening, ")
+	}
 }
